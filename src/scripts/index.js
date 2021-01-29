df2bddf80fbbb8c24ae3a9069198e4ce0a68f8a3
seungbee <seungbeelee@naver.com>
2021-01-29 23:14:57 +0900

    sprites

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,18 @@
 const canvas = document.getElementById("canvas")
 let ctx
 
-//if (canvas.getContext) { ctx = canvas.getContext('2d') } else { canvas.remove() }
+if (!canvas.getContext) { canvas.remove() }
 
 let Y, Yv, scene
-const main, tukim_high, tukim_mid, tukim_bottom = new Image()
+
+let TukimX = [360, 560, 760]
+let TukimY = [Math.floor(Math.random()*110), Math.floor(Math.random()*110), Math.floor(Math.random()*110)]
+
+const main = new Image()
+const tukim_high = new Image()
+const tukim_mid = new Image()
+const tukim_bottom = new Image()
+
 
 init()
 
@@ -37,17 +45,41 @@ function init(){
 function loop(){
   switch (scene) {
     case 1: {
-      draw1()
+      scene1()
       break
     }
     case 2: {
-      draw2()
+      scene2()
       break
     }
   }
 }
 
-function draw1(){
+
+function drawTukim(posX, posY, type){
+  switch(type){
+    case 1: {
+      ctx.drawImage(tukim_high, posX, posY, 50, 50)
+      for(i = 0; i < 8; i++){
+        ctx.drawImage(tukim_mid, posX, posY + (i+1) * 50, 50, 50)
+      }
+      break
+    }
+    case 0: {
+      ctx.drawImage(tukim_bottom, posX, posY, 50, 50)
+      for(i = 0; i < 8; i++){
+        ctx.drawImage(tukim_mid, posX, posY + (i+1) * -50, 50, 50)
+      }
+      break
+    }
+  }
+}
+function Tukim(posX, posY){
+  drawTukim(posX, posY, 0)
+  drawTukim(posX, posY+170, 1)
+}
+
+function scene1(){
   ctx = canvas.getContext('2d')
 
   ctx.imageSmoothingEnabled = false
@@ -61,10 +93,13 @@ function draw1(){
   
   ctx.clearRect(0, 0, 360, 540)
   ctx.drawImage(main, 60, Y, 50, 50)
+
+  Tukim(0,100)
   
+
 }
 
-function draw2(){
+function scene2(){
   ctx = canvas.getContext('2d')
   ctx.imageSmoothingEnabled = false
   ctx.clearRect(0, 0, 360, 540)
