62b27eb1895cd711e83e980b9ec7f4fffb7fe1aa
seungbee <seungbeelee@naver.com>
2021-01-30 00:55:19 +0900

    index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,7 @@ let ctx
 
 if (!canvas.getContext) { canvas.remove() }
 
-let Y, Yv, scene
-
-let TukimX = [360, 560, 760]
-let TukimY = [Math.floor(Math.random()*110), Math.floor(Math.random()*110), Math.floor(Math.random()*110)]
-
+let Y, Yv, scene, TukimX, TukimY, scoreGiven, score
 const main = new Image()
 const tukim_high = new Image()
 const tukim_mid = new Image()
@@ -22,11 +18,11 @@ function init(){
   tukim_mid.src = "src/sprites/tukim_mid.png"
   tukim_bottom.src = "src/sprites/tukim_bottom.png"
 
-  Y = 0
-  Yv = 0
-
   scene = 1
+
   
+
+  gameStart()
   setInterval(loop, 20)
 
   let cool = false
@@ -39,6 +35,15 @@ function init(){
     }
   })
   document.addEventListener('keyup', function(event) { if(event.keyCode === 32 || event.keyCode === 38 || event.keyCode === 87) return cool = false })
+}
+
+function gameStart(){
+  TukimX = [360, 560, 760]
+  TukimY = [Math.floor(Math.random()*290), Math.floor(Math.random()*290), Math.floor(Math.random()*290)]
+  Y = 0
+  Yv = 0
+  scoreGiven = false
+  score = 0
 }
 
 
@@ -76,7 +81,7 @@ function drawTukim(posX, posY, type){
 }
 function Tukim(posX, posY){
   drawTukim(posX, posY, 0)
-  drawTukim(posX, posY+170, 1)
+  drawTukim(posX, posY+185, 1)
 }
 
 function scene1(){
@@ -88,21 +93,60 @@ function scene1(){
   Y += Yv
   Yv += 1
 
-  if (Y >= 490 || Y < 0) scene = 2
+  if (Y >= 490 || Y < 0) { scene = 2 }
+
+  for (tukim_logic = 0; tukim_logic < 3; tukim_logic++){
+    if (TukimX[tukim_logic] < -250){
+      TukimX[tukim_logic] = 360
+      TukimY[tukim_logic] = Math.floor(Math.random()*290)
+      scoreGiven = false
+    }
+    else {TukimX[tukim_logic] -= 2}
+  }
+  for (tukim_collision = 0; tukim_collision < 3; tukim_collision++){
+    if (TukimX[tukim_collision] - 50 < 60 && TukimX[tukim_collision] + 50 > 60){
+      if (Y < TukimY[tukim_collision] + 50 || Y > TukimY[tukim_collision] + 135){
+        scene = 2
+        Yv = 0
+      }
+      else{
+        if (score == 1 && tukim_collision == 1) {score = 2}
+        if (!scoreGiven){
+          score ++
+          scoreGiven = true
+        }
+        
+      }
+    }
+  }
+
   //draw
   
   ctx.clearRect(0, 0, 360, 540)
   ctx.drawImage(main, 60, Y, 50, 50)
 
-  Tukim(0,100)
+  for (tukim_draw = 0; tukim_draw < 3; tukim_draw++) {
+    Tukim(TukimX[tukim_draw], TukimY[tukim_draw])
+  }
   
-
+  ctx.font = "30px Arial"
+  ctx.fillText(score, 170, 100)
 }
 
 function scene2(){
   ctx = canvas.getContext('2d')
   ctx.imageSmoothingEnabled = false
+
   ctx.clearRect(0, 0, 360, 540)
+
+  for (tukim_draw = 0; tukim_draw < 3; tukim_draw++) {
+    Tukim(TukimX[tukim_draw], TukimY[tukim_draw])
+  }
+  
+  ctx.fillStyle = "rgb(0,255,255)"
+  ctx.fillRect(140, 240, 100, 40)
+
+  ctx.fillStyle = "rgb(0,0,0)"
   ctx.font = "30px Arial"
   ctx.fillText("U ded", 150, 270)
 }
